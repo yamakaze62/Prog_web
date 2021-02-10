97858d7568ba024303822322ef347b12f6f707f5
yamakaze62 <34307104+yamakaze62@users.noreply.github.com>
2021-02-10 14:24:56 +0100

    3_comptagePHP

---
+++ Exercices/TP2/traite3.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Traitement3 du CSV</title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
+<body>
+<?php
+  $file = file("borneswifi_EPSG4326_20171004_utf8.csv");
+  $i = 0;
+  echo "<p>Table de CSV</p>";
+  echo '<table border="1">';
+  foreach ($file as $line_num => $line) {
+    echo "<tr><td>".$line."</td></tr>";
+    $i++;
+  }
+  echo "</table>";
+  echo "Nombre de point acces : ".$counter."<br />";
+ ?>
+</body>
+</html>
