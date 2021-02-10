a3c80fb6a5cac2701b4b4b01729f024039b1fa8c
yamakaze62 <34307104+yamakaze62@users.noreply.github.com>
2021-02-10 15:01:24 +0100

    3_comptagePHP

--- Exercices/TP2/traite3.php
+++ Exercices/TP2/traite3.php
@@ -6,15 +6,13 @@
 <body>
 <?php
   $file = file("borneswifi_EPSG4326_utf8.csv");
-  $i = 0;
   echo "<p>Table de CSV</p>";
   echo '<table border="1">';
   foreach ($file as $line_num => $line) {
     echo "<tr><td>".$line."</td></tr>";
-    $i++;
   }
   echo "</table>";
-  echo "Nombre de point acces : ".$counter."<br />";
+  echo "Nombre de point acces : ".$line_num."<br />";
  ?>
 </body>
 </html>
